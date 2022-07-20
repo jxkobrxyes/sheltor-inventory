a705cb2a18ed4b0b4c6b205e1cf6562720b348ce
SHJR1 <shhxuingccf@gmail.com>
2022-07-20 13:19:16 -0700

    Add getPetById route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,10 +5,11 @@ const Pet = require('../models/Pet');
 
 const router = express.Router();
 
-const { getAllPets } = require('../controllers/index');
+const { getAllPets, getPetById } = require('../controllers/index');
 
-//routes
+///routes
 router.get('*/pets', getAllPets);
+router.get('/pets/:id', getPetById);
 
 
 module.exports = router;
