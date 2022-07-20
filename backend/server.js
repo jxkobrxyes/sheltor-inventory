f7c99540aa4a12a2aba173396376638da7163c46
SHJR1 <shhxuingccf@gmail.com>
2022-07-20 10:41:08 -0700

    Fix db issue

--- backend/server.js
+++ backend/server.js
@@ -3,40 +3,40 @@ const cors = require('cors');
 const debug = require('debug')('app:server');
 const colors = require('colors');
 const path = require('path');
-const app = express();
 const sequelize = require('./src/db');
-const { Sequelize } = require("sequelize");
 
 //Import routes from src/routes folder
 //const routes = require('./src/routes/index');
 
 //check database connection
-// sequelize
-//   .authenticate()
-//   .then((res) => debug(colors.blue.inverse('Database is connected')))
-//   .catch((err) => {
-//     debug(
-//       colors.red.inverse('There was an error connecting to the database'),
-//       err
-//     );
-//     process.exit(1); //NODE TERMINATE SERVER
-//   });
+sequelize
+  .authenticate()
+  .then((res) => debug(colors.blue.inverse('Database is connected')))
+  .catch((err) => {
+    debug(
+      colors.red.inverse('There was an error connecting to the database'),
+      err
+    );
+    process.exit(1); //NODE TERMINATE SERVER
+  });
 
-
+const app = express();
 app.use(cors());
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src', 'public'))); //public
 
 const port = 8080;
 
+//routes
 //app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
-let yo = 'this is the connection';
+// let yo = 'this is the connection';
 
-app.get('/ayoo', (req, res) => {
-    res.send(yo)
-});
+// app.get('/ayoo', (req, res) => {
+//     res.send(yo)
+// });
