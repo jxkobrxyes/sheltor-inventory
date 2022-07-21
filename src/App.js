fe4aceb6f1ecd6ff2466d8a1a7d49c64d0e15766
aochil <azizbek.ochilov2@gmail.com>
2022-07-21 13:43:54 -0400

    added routes to each page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect } from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/nav-bar/navBar.jsx';
+import Home from './pages/Home';
+import PetInfo from './pages/PetInfo';
+import CreatePet from './pages/CreatePet';
+
 
 function App() {
-
-
   return (
-    <div classNme = 'container'>
+    <Router>
       <NavBar />
-    </div>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pets/:id' element={<PetInfo/>} />
+          <Route path='/pets/create' element={<CreatePet/>} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
