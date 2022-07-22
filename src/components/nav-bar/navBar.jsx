fc136c25067e5d4a34ce908483baafe8ee749f7d
Jakob Reyes <jxreyes521@gmail.com>
2022-07-22 08:38:42 -0700

    finished navbar

--- src/components/nav-bar/navBar.jsx
+++ src/components/nav-bar/navBar.jsx
@@ -1,18 +1,29 @@
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './navBar.scss';
-
 
 const NavBar = () =>{
     return(
-        <div className = 'nav-bar-container'>
-            <h1> I am the navbar</h1>
-        </div>
+        <>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <img classname ='logo' src="/images/animal-shelter.png" alt="pet shelter" width={40} height={40}/>
+                    <ul className='menu'>
+                        <li className='item'>
+                            <Link to='/'classname='link'>Home</Link>
+                        </li>
+                        <li className='item'>
+                            <Link to='/pets'classname='link'>Pets</Link>
+                        </li>
+                        <li className='item'>
+                            <Link to='/'classname='link'>About</Link>
+                        </li>
+                        <Link to="/"><button id='Admin'>Admin</button></Link>
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 };
-
-
-
-
 
 export default NavBar
